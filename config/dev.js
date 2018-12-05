e108ddb96788b5902f7f2eb0387c555ab14d28a2
eulises22 <42219386+eulises22@users.noreply.github.com>
2018-12-05 14:12:02 -0800

    Add files via upload

--- config/dev.js
+++ config/dev.js
@@ -1,1 +1,11 @@
+// dev.js
+// dev.js
+// dev .js
 
+module.exports = 
+{
+    googleClientID: '808829131515-253d90lfi5q1hdql9e9ltor6tibj8uu8.apps.googleusercontent.com',
+    googleClientSecret: '0rXGjmoXuzaxnHbs85zkQmA-',
+    mongoURI: 'mongodb://ulyses:password1@ds211724.mlab.com:11724/emaily-dev',
+    cookieKey: 'aaaaa'
+};
